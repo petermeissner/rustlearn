162ee718d920eb1784fb4079f461dc0332cfcc1a
petermeissner <retep.meissner@gmail.com>
2022-08-21 22:28:28 +0200

    fix parsing of input

--- examples/04_random_number.rs
+++ examples/04_random_number.rs
@@ -10,8 +10,7 @@ fn main() {
     io::stdin()
     .read_line(&mut n)
     .expect("Failed to read line");
-    let n_u32: u32 = "7".parse().expect("Parsing Failed");
-    // TODO!!! : parsing does not work, yet ???!!!
+    let n_u32: u32 = n.trim().parse().expect("Parsing Failed");
     println!("N: {n_u32}");
     
     
@@ -20,19 +19,19 @@ fn main() {
     io::stdin()
     .read_line(&mut start)
     .expect("Failed to read line");
-    // TODO!!! : parsing does not work, yet ???!!!
-    let start_u32: u32 = "1".parse().unwrap();
+    let start_u32: u32 = start.trim().parse().unwrap();
     
     println!("End?:");
     let mut end = String::new();
     io::stdin()
     .read_line(&mut end)
     .expect("Failed to read line");
-    // TODO!!! : parsing does not work, yet ???!!!
-    let end_u32: u32 = "6".parse().unwrap();
+    let end_u32: u32 = end.trim().parse().unwrap();
     
     for i in 0..n_u32 {
-      let secret_number: u32 = rand::thread_rng().gen_range(start_u32..end_u32);
+      let secret_number: u32 = 
+        rand::thread_rng().
+        gen_range(start_u32..(end_u32+1));
       let ii = i+1;
       println!("{ii}: {secret_number}");
     }
